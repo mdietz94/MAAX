aee46495c3938183623e99a5b8c795c2bc84109a
Max Dietz <mdietz@wesleyan.edu>
2015-06-23 09:32:40 -0400

    now compiling on linux AND linking to Haskell!

--- src/Emulator.hs
+++ src/Emulator.hs
@@ -1,28 +1,27 @@
-{-# INCLUDE <emulator.h> #-}
-{-# LANGUAGE ForeignFunctionInteface #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 module Emulator where
-	import Data.List
-	import Data.Word
-	import qualified Data.Vector.Unboxed as V
-	import Foreign.Ptr
-	import Foreign.Storable
-	import Foreign.C.Types
-	import Foreign.C.String
-	import Foreign.Marshal.Array
-	import Foreign.Marshal.Alloc
+    import Data.List
+    import Data.Word
+    import Foreign.Ptr
+    import Foreign.Storable
+    import Foreign.C.Types
+    import Foreign.C.String
+    import Foreign.Marshal.Array
+    import Foreign.Marshal.Alloc
+    import Data.Bits.Bitwise (fromListBE)
 
-	data Joystick = Joystick { right  :: Bool
-	                         , left   :: Bool
-	                         , down   :: Bool
-	                         , up     :: Bool
-	                         , start  :: Bool
-	                         , select :: Bool
-	                         , b      :: Bool
-	                         , a      :: Bool }
+    data Joystick = Joystick { right  :: Bool
+                             , left   :: Bool
+                             , down   :: Bool
+                             , up     :: Bool
+                             , start  :: Bool
+                             , select :: Bool
+                             , b      :: Bool
+                             , a      :: Bool }
 
 
     joystickToChar :: Joystick -> CUChar
-    joystickToChar r l d u st s b a = fromListBE [r,l,d,u,st,s,b,a]
+    joystickToChar (Joystick r l d u st s b a) = fromListBE [r,l,d,u,st,s,b,a]
 
 
     {-
@@ -34,13 +33,13 @@ module Emulator where
 
     -}
 
-	foreign import ccall "Create" create :: (CString -> (Ptr CUChar) -> IO ())
-	foreign import ccall "Destroy" destroy :: (IO ())
-	foreign import ccall "Save" save :: ((Ptr Emulator) -> (Ptr CUChar))
-	foreign import ccall "Load" load :: (IO ())
-	foreign import ccall "GetMemory" getMemory :: (Ptr CUChar)
-	foreign import ccall "GetImage" getImage :: (Ptr CUChar)
-	foreign import ccall "Step" step :: (IO ())
-	foreign import ccall "StepFull" stepFull :: (IO ())
-	foreign import ccall "RamChecksum" ramChecksum :: (IO CLong)
-	foreign import ccall "ImageChecksum" ImageChecksum :: (IO CLong)
+    foreign import ccall "Create" create :: (CString -> (Ptr CUChar) -> IO ())
+    foreign import ccall "Destroy" destroy :: (IO ())
+    foreign import ccall "Save" save :: (Ptr CUChar)
+    foreign import ccall "Load" load :: (IO ())
+    foreign import ccall "GetMemory" getMemory :: (Ptr CUChar)
+    foreign import ccall "GetImage" getImage :: (Ptr CUChar)
+    foreign import ccall "Step" step :: (IO ())
+    foreign import ccall "StepFull" stepFull :: (IO ())
+    foreign import ccall "RamChecksum" ramChecksum :: (IO CLong)
+    foreign import ccall "ImageChecksum" imageChecksum :: (IO CLong)
---
+++ src/Main.hs
@@ -0,0 +1,3 @@
+import Emulator
+
+main = putStrLn "Hello"
