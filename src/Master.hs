7c3e6d29a6190b40c3f62a5f9c019022dd8806bf
Justin Raymond <justin.raymond.14@gmail.com>
2015-07-10 15:49:33 -0400

    master worker ok

--- src/Master.hs
+++ src/Master.hs
@@ -1,11 +1,43 @@
 {-# LANGUAGE OverloadedStrings #-}
+
+import Mario
 import Types
 
+import Control.Concurrent
+import Control.Concurrent.Async
+import Control.Lens
+import Data.Binary
+import qualified Data.ByteString.Lazy as B
 import Network.Socket
+import System.IO
 
 main = do
+  addr <- inet_addr "127.0.0.1"
+  i <- sendGenome addr 0 testGenome
+  return ()
+
+
+sendGenome :: HostAddress -> Int -> Genome -> IO Genome
+sendGenome addr uid g = do
   sock <- socket AF_INET Stream 0
-  worker <- inet_addr "127.0.0.1"
-  connect sock (SockAddrInet 3000 worker)
-  send sock "hello"
-  sClose sock
+  connect sock (SockAddrInet 3000 addr)
+  handle <- socketToHandle sock ReadWriteMode
+  B.hPut handle (encode g)
+  result <- decode <$> B.hGetContents handle :: IO Genome
+  print result
+  hClose handle
+  return result
+
+
+runMaster :: [HostAddress] -> [(Int,Genome)] -> IO [Int]
+runMaster _ [] = return []
+runMaster workers gs = do
+  fs <- mapConcurrently (\(w,(uid,g)) -> sendGenome w uid g) (zip workers gs)
+  runMaster workers (drop (length workers) gs)
+
+testGenome :: Genome
+testGenome = Genome 0 (om + 1) gs1 where
+  im = 169 - 1
+  om = 169 + 6
+  gs0 = [Gene i o 1 True 0 | i <- [0 .. im], o <- [im + 1 .. om]]
+  gs1 = zipWith (set innovation) [0..] gs0
--- src/Worker.hs
+++ src/Worker.hs
@@ -4,10 +4,13 @@ import qualified Mario as M
 
 import Debug.Trace
 
+import Data.Binary
+import qualified Data.ByteString.Lazy as B
 import Network.Socket
 import Control.Concurrent
 import System.IO
- 
+
+
 main = withSocketsDo $ do
     sock <- socket AF_INET Stream 0
     setSocketOption sock ReuseAddr 1
@@ -15,18 +18,21 @@ main = withSocketsDo $ do
     listen sock 2
     loop sock
 
+
 loop :: Socket -> IO ()
 loop sock = do
   conn <- accept sock
   forkIO (runConn conn)
   loop sock
 
+
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock,_) = do
   handle <- socketToHandle sock ReadWriteMode
   hSetBuffering handle NoBuffering
-  cnts <- hGetContents handle
-  print cnts
+  g0 <- decode <$> B.hGetContents handle :: IO Genome
+  g1 <- M.runMario g0
+  B.hPut handle (encode g1)
   hClose handle
 
  
