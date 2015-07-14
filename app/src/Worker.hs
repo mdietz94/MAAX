cf04f5dfdc1e6b05632cb4648e978df810e9878a
Justin Raymond <justin.raymond.14@gmail.com>
2015-07-14 11:13:01 -0400

    added retries as cmd arg

--- app/src/Worker.hs
+++ app/src/Worker.hs
@@ -37,7 +37,7 @@ startWorker opts = withSocketsDo $ do
           setFormatter lh (simpleLogFormatter "[$time : $prio] $msg")
   updateGlobalLogger rootLoggerName (setLevel INFO . addHandler logH)
   infoM rootLoggerName "Worker starting up"
-  loop opts 10 10 1
+  loop opts (_retries opts) (_retries opts) 1
 
 
 -- t is how long to wait before retrying in seconds
@@ -53,7 +53,7 @@ loop opts nmax n t = catch (loopListen opts) handler
                     loop opts nmax nmax 1
 
 loopListen :: Opts -> IO ()
-loopListen (Opts ip port _) = do
+loopListen (Opts ip port _ _) = do
     infos <- getAddrInfo Nothing (Just ip) (Just port)
     sock <- socket AF_INET Stream 0
     connect sock (addrAddress . head $ infos)
@@ -85,25 +85,25 @@ strictDecode = decode . BL.fromStrict
 
 data Opts = Opts { _ip :: String
                  , _port :: String 
-                 , _logF :: String }
+                 , _logF :: String
+                 , _retries :: Int }
 
 instance Show Opts where
-  show (Opts ip port _) = ip ++ ':' : port
+  show (Opts ip port _ _) = ip ++ ':' : port
 
 optsParser :: Parser Opts
 optsParser = 
-    Opts <$> strOption (short 'm' <>
-                       long "master" <>
-                       metavar "MASTER_IP" <>
-                       value "joomah.com" <>
+    Opts <$> strOption (short 'm' <> long "master" <>
+                       metavar "MASTER_IP" <> value "joomah.com" <>
                        help "IP of master, default joomah.com")
-         <*> strOption (short 'p' <>
-                       long "port" <>
-                       metavar "MASTER_PORT" <>
-                       value "3000" <>
+         <*> strOption (short 'p' <> long "port" <>
+                       metavar "MASTER_PORT" <> value "3000" <>
                        help "Port master is listening on, default 3000")
-         <*> strOption (short 'l' <>
-                       long "log" <>
-                       metavar "FILE" <>
-                       value "worker.log" <>
+         <*> strOption (short 'l' <> long "log" <>
+                       metavar "FILE" <> value "worker.log" <>
                        help "File to log messages to, default worker.log")
+         <*> option auto (short 'r' <> long "retries" <>
+                         metavar "RETRIES" <> value 10 <>
+                         help "Retries after connection refused")
+
+
