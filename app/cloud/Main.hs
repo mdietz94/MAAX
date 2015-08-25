3f32e7a6e3650c9c81cc3b82a3a59dca0aa76f13
Justin Raymond <justin.raymond.14@gmail.com>
2015-08-25 12:58:57 -0400

    refactor

--- app/cloud/Main.hs
+++ app/cloud/Main.hs
@@ -6,20 +6,19 @@ import qualified Mario as M
 import NeuralNetwork (loadPopulation, savePopulation, fittestGenome, marioConfig)
 import Emulator (saveAsFM2)
 
-import qualified Control.Concurrent as C
+import Control.Concurrent (threadDelay)
 import Control.Distributed.Process hiding (Message)
 import Control.Distributed.Process.Closure
-import Control.Monad
-import Control.Monad.Loops
-import Data.List (sortOn)
---import qualified Data.Vector.Unboxed as U
-import qualified Data.Vector.Unboxed.Mutable as MU
-import Text.Printf
-import Data.Binary
-import Data.Typeable
-import GHC.Generics (Generic)
 import Control.Distributed.Process.Node (initRemoteTable)
 import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Lens (over,(^.))
+import Control.Monad
+import Text.Printf
+import Data.Binary
+import qualified Data.Map.Strict as Map
+import Data.List (sortOn)
+import Data.Typeable
+import GHC.Generics (Generic)
 import System.Environment
 import System.Directory
 import System.Random
@@ -56,30 +55,33 @@ distribMain master frtable = do
 
 
 data Message = MsgServers [ProcessId]
-             | MsgIdle
-             | MsgG ProcessId (Int,Genome)
-             | MsgBroadcast Message
+             | MsgInit ProcessId
+             | MsgNoWork ProcessId
+             | MsgGenome ProcessId (Int,Genome)
+             | MsgFitness ProcessId (Maybe (Int,Float))
              deriving (Typeable,Generic)
 
 instance Binary Message
 
-
-
-{- The clients wait for genomes, run the genomes, then send them back -}
+{- The clients wait for genomes, run the genomes, then send the fitness
+- back -}
 pingServer :: Process ()
-pingServer =
-  forever $ do
-    MsgG from (gid,genome) <- expect
-    mypid <- getSelfPid
-    genome' <- liftIO $ M.runMario genome
-    send from (MsgG mypid (gid,genome'))
+pingServer = do
+  MsgInit from <- expect
+  mypid <- getSelfPid
+  send from (MsgFitness mypid Nothing)
+  forever $
+    receiveWait 
+      [ match $ \(MsgNoWork _) -> do
+          liftIO $ threadDelay 1000000
+          send from (MsgFitness mypid Nothing)
+      , match $ \(MsgGenome _ (gid,genome0)) -> do
+          genome <- liftIO $ M.runMario genome0
+          send from (MsgFitness mypid (Just (gid,genome^.fitness)))
+      ]
 
 remotable ['pingServer]
 
-{- The use of MVars in this function is NOT thread safe. The only reason they're
-- is because legacy and I'm too lazy to change them to a mutable value, but not
-- so lazy as to not write this comment.
--}
 master :: Config -> [NodeId] -> Process ()
 master _ peers = do
 
@@ -90,60 +92,64 @@ master _ peers = do
           say $ printf "spawing on %s" (show nid)
           spawn nid $(mkStaticClosure 'pingServer)
   mypid <- getSelfPid
+  forM_ ps $ \pid -> send pid (MsgInit mypid)
 
-  mapM_ monitor ps
+  mapM_ monitor ps --will let us know if slave fails
 
   (num,p0) <- liftIO loadMaxPopulation
-  let rs = randomRs (0.0,1.0) (mkStdGen 42)
-  _ <- runPopulation mypid ps (0,p0,rs) num
+  _ <- runPopulation (0,p0) num
 
   say "all done"
   terminate
 
-sender :: ProcessId
-       -> [ProcessId]
-       -> MU.IOVector Bool
-       -> Int
-       -> C.MVar Int
-       -> C.MVar Int
-       -> C.Chan (Int,Genome)
-       -> C.Chan (Int,Genome)
-       -> Process ()
-sender mypid ps ready ntodo nsent ndone todo done =
-  whileM_ (liftIO ((< ntodo) <$> C.readMVar ndone)) $ do
-    sendAll mypid ps ready ntodo nsent todo
-    receiveWait 
-      [ match $ \(ProcessMonitorNotification ref pid reason) ->
-          say (show pid ++ " died: " ++ show reason ++ " ref: " ++ show ref) --TODO cleanup this
-      , match $ \(MsgG _ g') -> do
-          liftIO $ C.writeChan done g'
-          liftIO $ incMVar ndone
-      ]
-   
-sendAll :: ProcessId -> [ProcessId] -> MU.IOVector Bool -> Int -> C.MVar Int -> C.Chan (Int,Genome) -> Process ()
-sendAll mypid ps ready ntodo nsent todo =
-  forM_ (zip [0..] ps) $ \(i,pid) -> do
-    idle <- liftIO $ MU.read ready i
-    numsent <- liftIO $ C.readMVar nsent
-    when (idle && numsent < ntodo) $ do
-      g <- liftIO $ C.readChan todo
-      send pid (MsgG mypid g)
-      liftIO $ incMVar nsent
-      liftIO $ MU.write ready i False
+runPopulation :: (Int,Population) -> Int -> Process b
+runPopulation (gInnov,p0) n = do
+  p1 <- run p0
+  liftIO $ savePopulation ("./data/" ++ show n ++ ".bin") p1
+  rgen <- liftIO newStdGen
+  let rfs = randomRs (0.0,1.0) rgen
+      (gInnov',p2,_) = M.stepNetwork p0 (gInnov,p1,rfs) marioConfig
+  liftIO $ savePopulation ("./data/" ++ show (n+1) ++ ".bin") p2
+  joydata <- liftIO $ M.recordMario (fittestGenome p2)
+  liftIO $ saveAsFM2 ("./data/" ++ show (n+1) ++ ".fm2") joydata
+  runPopulation (gInnov',p2) (n+1)
 
-recAll :: [ReceivePort a] -> Process [t]
-recAll [] = return []
-recAll (port:ps) =
-  receiveWait
-    [ match $ \(ProcessMonitorNotification ref pid reason) -> do
-        say (show pid ++ " died: " ++ show reason ++ "\tref: " ++ show ref)
-        recAll (port:ps)
-    , matchChan port $ \_ -> do
-       say "pong on channel"
-       recAll ps
-    ]
+run :: Population -> Process Population
+run pop = do
+  let sizes = map (\(_,_,_,_,gs) -> length gs) pop
+      genomes0 = concatMap (\(_,_,_,_,gs) -> gs) pop
+  genomes' <- sendAll genomes0
+  let genomes'' = splitInto sizes genomes'
+      pop' = zipWith (\(a,b,c,d,_) gs -> (a,b,c,d,gs)) pop genomes''
+  return pop'
 
-
+sendAll :: [Genome] -> Process [Genome]
+sendAll todo0 = do
+  mypid <- getSelfPid
+  fs <- map snd . sortOn fst <$> loop mypid (zip [0..] todo0) [] Map.empty
+  return $ zipWith (over fitness . const) fs todo0
+  where
+    ntodo = length todo0
+    loop mypid todo done info
+      | length done == ntodo = return done
+      | otherwise =
+        receiveWait
+          [ match $ \(ProcessMonitorNotification ref pid reason) -> do
+              say (show pid ++ " ref: " ++ show ref ++ " died: " ++ show reason)
+              case Map.lookup pid info of
+                Nothing -> loop mypid todo done info
+                (Just w) -> loop mypid (w : todo) done (Map.delete pid info)
+          , match $ \(MsgFitness from g) -> do
+              let done' = maybe done (:done) g
+              if not (null todo)
+                then do
+                  let (work : todo') = todo
+                  send from (MsgGenome mypid work)
+                  loop mypid todo' done' (Map.insert from work info)
+                else do
+                  send from (MsgNoWork mypid)
+                  loop mypid todo done' (Map.delete from info)
+          ]
 
 loadMaxPopulation :: IO (Int, Population)
 loadMaxPopulation = loadMaxPop 1
@@ -155,43 +161,6 @@ loadMaxPopulation = loadMaxPop 1
          else do
            p0 <- loadPopulation $ "./data/" ++ show (n-1) ++ ".bin"
            return (n-1,p0)
-
-readNextN :: C.Chan a -> Int -> IO [a]
-readNextN _ 0 = return []
-readNextN ch n = C.readChan ch >>= \x -> (x:) <$> readNextN ch (n - 1)
-
-incMVar :: C.MVar Int -> IO ()
-incMVar mv = do
-  i <- C.takeMVar mv
-  C.putMVar mv (i + 1)
-
-runPopulation :: ProcessId -> [ProcessId] -> (Int,Population,[Float]) -> Int -> Process b
-runPopulation mypid ps (gInnov,p0,gen) n = do
-  p1 <- run mypid ps p0
-  liftIO $ savePopulation ("./data/" ++ show n ++ ".bin") p1
-  let (gInnov',p2,gen') = M.stepNetwork p0 (gInnov,p1,gen) marioConfig
-  liftIO $ savePopulation ("./data/" ++ show (n+1) ++ ".bin") p2
-  joydata <- liftIO $ M.recordMario (fittestGenome p2)
-  liftIO $ saveAsFM2 ("./data/" ++ show (n+1) ++ ".fm2") joydata
-  runPopulation mypid ps (gInnov',p2,gen') (n+1)
-
---run :: [Genome] -> Socket -> IO [Genome]
-run :: ProcessId -> [ProcessId] -> Population -> Process Population
-run mypid ps pop = do
-  let sizes = map (\(_,_,_,_,gs) -> length gs) pop
-      genomes = concatMap (\(_,_,_,_,gs) -> gs) pop
-      ntodo = length genomes
-  ndone <- liftIO $ C.newMVar 0
-  nsent <- liftIO $ C.newMVar 0
-  todo <- liftIO (C.newChan :: IO (C.Chan (Int,Genome)))
-  done <- liftIO (C.newChan :: IO (C.Chan (Int,Genome)))
-  ready <- liftIO $ MU.replicate (length ps) True
-  liftIO $ C.writeList2Chan todo (zip [0..] genomes)
-  sender mypid ps ready ntodo nsent ndone todo done
-  genomes' <- liftIO $ map snd . sortOn fst <$> readNextN done ntodo
-  let genomes'' = splitInto sizes genomes'
-      pop' = zipWith (\(a,b,c,d,_) gs -> (a,b,c,d,gs)) pop genomes''
-  return pop'
 
 splitInto :: [Int] -> [a] -> [[a]]
 splitInto [] as = [as]
