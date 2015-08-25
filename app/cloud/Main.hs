04b982d87a98b88994a1d448ab386b18c1e6bf71
Justin Raymond <justin.raymond.14@gmail.com>
2015-08-25 11:20:28 -0400

    compiles

--- app/cloud/Main.hs
+++ app/cloud/Main.hs
@@ -1,19 +1,18 @@
 {-# LANGUAGE TemplateHaskell, DeriveDataTypeable, DeriveGeneric #-}
 module Main where
 
-import Types
+import Types hiding (genome)
 import qualified Mario as M
 import NeuralNetwork (loadPopulation, savePopulation, fittestGenome, marioConfig)
 import Emulator (saveAsFM2)
 
 import qualified Control.Concurrent as C
-import Control.Concurrent.STM.TVar
 import Control.Distributed.Process hiding (Message)
 import Control.Distributed.Process.Closure
 import Control.Monad
 import Control.Monad.Loops
 import Data.List (sortOn)
-import qualified Data.Vector.Unboxed as U
+--import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as MU
 import Text.Printf
 import Data.Binary
@@ -23,6 +22,7 @@ import Control.Distributed.Process.Node (initRemoteTable)
 import Control.Distributed.Process.Backend.SimpleLocalnet
 import System.Environment
 import System.Directory
+import System.Random
 
 distribMain :: ([NodeId] -> Process ()) -> (RemoteTable -> RemoteTable) -> IO ()
 distribMain master frtable = do
@@ -81,7 +81,7 @@ remotable ['pingServer]
 - so lazy as to not write this comment.
 -}
 master :: Config -> [NodeId] -> Process ()
-master c peers = do
+master _ peers = do
 
   liftIO $ mapM_ print peers
 
@@ -93,42 +93,52 @@ master c peers = do
 
   mapM_ monitor ps
 
-
   (num,p0) <- liftIO loadMaxPopulation
-  runPopulation ps p0 num
+  let rs = randomRs (0.0,1.0) (mkStdGen 42)
+  _ <- runPopulation mypid ps (0,p0,rs) num
 
   say "all done"
   terminate
 
-
+sender :: ProcessId
+       -> [ProcessId]
+       -> MU.IOVector Bool
+       -> Int
+       -> C.MVar Int
+       -> C.MVar Int
+       -> C.Chan (Int,Genome)
+       -> C.Chan (Int,Genome)
+       -> Process ()
 sender mypid ps ready ntodo nsent ndone todo done =
   whileM_ (liftIO ((< ntodo) <$> C.readMVar ndone)) $ do
     sendAll mypid ps ready ntodo nsent todo
     receiveWait 
       [ match $ \(ProcessMonitorNotification ref pid reason) ->
-          say (show pid ++ " died: " ++ show reason) --TODO cleanup this
-      , match $ \(MsgG pid g') -> do
-          C.writeChan done g'
+          say (show pid ++ " died: " ++ show reason ++ " ref: " ++ show ref) --TODO cleanup this
+      , match $ \(MsgG _ g') -> do
+          liftIO $ C.writeChan done g'
           liftIO $ incMVar ndone
       ]
-    
+   
+sendAll :: ProcessId -> [ProcessId] -> MU.IOVector Bool -> Int -> C.MVar Int -> C.Chan (Int,Genome) -> Process ()
 sendAll mypid ps ready ntodo nsent todo =
   forM_ (zip [0..] ps) $ \(i,pid) -> do
     idle <- liftIO $ MU.read ready i
     numsent <- liftIO $ C.readMVar nsent
     when (idle && numsent < ntodo) $ do
       g <- liftIO $ C.readChan todo
-      send pid (MsgG mypid genome)
+      send pid (MsgG mypid g)
       liftIO $ incMVar nsent
       liftIO $ MU.write ready i False
 
+recAll :: [ReceivePort a] -> Process [t]
 recAll [] = return []
 recAll (port:ps) =
   receiveWait
     [ match $ \(ProcessMonitorNotification ref pid reason) -> do
-        say (show pid ++ " died: " ++ show reason)
+        say (show pid ++ " died: " ++ show reason ++ "\tref: " ++ show ref)
         recAll (port:ps)
-    , matchChan port $ \p -> do
+    , matchChan port $ \_ -> do
        say "pong on channel"
        recAll ps
     ]
@@ -155,45 +165,38 @@ incMVar mv = do
   i <- C.takeMVar mv
   C.putMVar mv (i + 1)
 
-replaceGenomes :: Population -> [Genome] -> Population
-replaceGenomes pop gns = map (\(a,b,c,d,gs) -> (a,b,c,d,replace gs gns)) pop
-  where
-    replace :: [Genome] -> [Genome] -> [Genome]
-    replace [] _ = []
-    replace g [] = g
-    replace (x:xs) gs = fndMatch x gs : replace xs gs
-    fndMatch :: Genome -> [Genome] -> Genome
-    fndMatch x [] = x
-    fndMatch x (g:gs)
-      | x `eq` g = g
-      | otherwise = fndMatch x gs
-    eq (Genome _ x y) (Genome _ x1 y1) = x == x1 && y == y1
-
-
---runPopulation :: (Int, Population, [Float]) -> Int -> Socket -> IO b
-runPopulation peers (gInnov,p0,gen) n = do
-  let genomes = concatMap (\(_,_,_,_,gs) -> gs) p0
-  genomes' <- run peers genomes
-  let p1 = replaceGenomes p0 genomes'
-  savePopulation ("./data/" ++ show n ++ ".bin") p1
+runPopulation :: ProcessId -> [ProcessId] -> (Int,Population,[Float]) -> Int -> Process b
+runPopulation mypid ps (gInnov,p0,gen) n = do
+  p1 <- run mypid ps p0
+  liftIO $ savePopulation ("./data/" ++ show n ++ ".bin") p1
   let (gInnov',p2,gen') = M.stepNetwork p0 (gInnov,p1,gen) marioConfig
-  savePopulation ("./data/" ++ show (n+1) ++ ".bin") p2
-  joydata <- M.recordMario (fittestGenome p2)
-  saveAsFM2 ("./data/" ++ show (n+1) ++ ".fm2") joydata
-  runPopulation peers (gInnov',p2,gen') (n+1)
+  liftIO $ savePopulation ("./data/" ++ show (n+1) ++ ".bin") p2
+  joydata <- liftIO $ M.recordMario (fittestGenome p2)
+  liftIO $ saveAsFM2 ("./data/" ++ show (n+1) ++ ".fm2") joydata
+  runPopulation mypid ps (gInnov',p2,gen') (n+1)
 
 --run :: [Genome] -> Socket -> IO [Genome]
-run :: [ProcessId] -> [Genome] -> Process [Genome]
-run ps genomes = do
-  let ntodo = length genomes
+run :: ProcessId -> [ProcessId] -> Population -> Process Population
+run mypid ps pop = do
+  let sizes = map (\(_,_,_,_,gs) -> length gs) pop
+      genomes = concatMap (\(_,_,_,_,gs) -> gs) pop
+      ntodo = length genomes
   ndone <- liftIO $ C.newMVar 0
+  nsent <- liftIO $ C.newMVar 0
   todo <- liftIO (C.newChan :: IO (C.Chan (Int,Genome)))
   done <- liftIO (C.newChan :: IO (C.Chan (Int,Genome)))
   ready <- liftIO $ MU.replicate (length ps) True
   liftIO $ C.writeList2Chan todo (zip [0..] genomes)
-  sender ps ntodo ndone todo done ps
-  liftIO $ snd <$> readNextN done ntodo
+  sender mypid ps ready ntodo nsent ndone todo done
+  genomes' <- liftIO $ map snd . sortOn fst <$> readNextN done ntodo
+  let genomes'' = splitInto sizes genomes'
+      pop' = zipWith (\(a,b,c,d,_) gs -> (a,b,c,d,gs)) pop genomes''
+  return pop'
+
+splitInto :: [Int] -> [a] -> [[a]]
+splitInto [] as = [as]
+splitInto (i:is) as = let (xs,ys) = splitAt i as in xs : splitInto is ys
 
 
 main :: IO ()
-main = distribMain master Main.__remoteTable
+main = distribMain (master marioConfig) Main.__remoteTable
